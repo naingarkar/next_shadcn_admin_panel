45fc5aeb094f1a0acdf831a108f5c72e650c10bb
Naing Arkar Win <naingarkarwin.bit@gmail.com>
2024-06-29 04:06:07 +0700

    add next auth login with custom page

---
+++ src/app/provider.tsx
@@ -0,0 +1,12 @@
+import { SessionProvider } from 'next-auth/react'
+import React from 'react'
+
+const Provider = ({children, session} : {children: any, session?: any}) => {
+  return (
+    <SessionProvider session={session}>
+      {children}
+    </SessionProvider>
+  )
+}
+
+export default Provider
