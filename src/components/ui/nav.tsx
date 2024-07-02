04eb00bedda86fc903577f3884ccc7735aa37727
Naing Arkar Win <naingarkarwin.bit@gmail.com>
2024-07-03 01:56:20 +0700

    add signout button

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -31,50 +31,50 @@ export function Nav({ links }: NavProps) {
         <div
         className="group flex flex-col gap-4 py-2"
         >
-        <nav className="grid gap-1 px-2 ">
-            {links.map((link, index) =>
-                <div key={index}>
-                    <div className="block md:hidden">
-                        <Tooltip key={index} delayDuration={0}>
-                            <TooltipTrigger asChild>
-                                <Link
-                                href={link.href}
-                                className={cn(
-                                    buttonVariants({ variant: link.href === pathName ? "default" : "ghost", size: "icon" }),
-                                    "h-9 w-9",
-                                    link.variant === "default" &&
-                                    "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
-                                )}
-                                >
-                                <link.icon className="h-4 w-4" />
-                                <span className="sr-only">{link.title}</span>
-                                </Link>
-                            </TooltipTrigger>
-                            <TooltipContent side="right" className="flex items-center gap-4">
-                                {link.title}
-                               
-                            </TooltipContent>
-                        </Tooltip>
+            <nav className="grid gap-1 px-2 ">
+                {links.map((link, index) =>
+                    <div key={index}>
+                        <div className="block md:hidden">
+                            <Tooltip key={index} delayDuration={0}>
+                                <TooltipTrigger asChild>
+                                    <Link
+                                    href={link.href}
+                                    className={cn(
+                                        buttonVariants({ variant: link.href === pathName ? "default" : "ghost", size: "icon" }),
+                                        "h-9 w-9",
+                                        link.variant === "default" &&
+                                        "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                                    )}
+                                    >
+                                    <link.icon className="h-4 w-4" />
+                                    <span className="sr-only">{link.title}</span>
+                                    </Link>
+                                </TooltipTrigger>
+                                <TooltipContent side="right" className="flex items-center gap-4">
+                                    {link.title}
+                                
+                                </TooltipContent>
+                            </Tooltip>
+                        </div>
+                
+                        <div className="hidden md:block">
+                            <Link
+                            key={index}
+                            href={link.href}
+                            className={cn(
+                                buttonVariants({  variant: link.href === pathName ? "default" : "ghost", size: "sm" }),
+                                link.variant === "default" &&
+                                "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
+                                "justify-start"
+                            )}
+                            >
+                            <link.icon className="mr-2 h-4 w-4" />
+                            {link.title}
+                            </Link>
+                        </div>
                     </div>
-            
-                    <div className="hidden md:block">
-                        <Link
-                        key={index}
-                        href={link.href}
-                        className={cn(
-                            buttonVariants({  variant: link.href === pathName ? "default" : "ghost", size: "sm" }),
-                            link.variant === "default" &&
-                            "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
-                            "justify-start"
-                        )}
-                        >
-                        <link.icon className="mr-2 h-4 w-4" />
-                        {link.title}
-                        </Link>
-                    </div>
-                </div>
-            )}
-        </nav>
+                )}
+            </nav>
         </div>
     </TooltipProvider>
   )
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -10,6 +10,7 @@ import {
     File,
     Inbox,
     LayoutDashboard,
+    LogOutIcon,
     MessagesSquare,
     Search,
     Send,
@@ -18,6 +19,8 @@ import {
     Users2,
   } from "lucide-react"
 import { Button } from './button'
+import Link from 'next/link'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 
 const Sidebar = () => {
     return (
@@ -30,9 +33,57 @@ const Sidebar = () => {
                     icon: LayoutDashboard,
                     variant: "default",
                     label: "Dashboard"
+                },
+                {
+                    title: "Test",
+                    href: "/test",
+                    icon: LayoutDashboard,
+                    variant: "default",
+                    label: "test"
+                },
+                {
+                    title: "Signout",
+                    href: "/signout",
+                    icon: LogOutIcon,
+                    variant: "default",
+                    label: "signout"
                 }
                 ]}
             />
+            {/* <TooltipProvider>
+                <div
+                className="group flex flex-col gap-4 py-2"
+                >
+                    <nav className="grid gap-1 px-2 ">
+                        <div className="block md:hidden">
+                            <Tooltip delayDuration={0}>
+                                <TooltipTrigger asChild>
+                                    <Link
+                                    href="#"
+                                    className="h-9 w-9 ghost icon"
+                                    >
+                                    <LogOutIcon className="h-4 w-4" />
+                                    <span className="sr-only">Signout</span>
+                                    </Link>
+                                </TooltipTrigger>
+                                <TooltipContent side="right" className="flex items-center gap-4">
+                                    Signout
+                                </TooltipContent>
+                            </Tooltip>
+                        </div>
+                
+                        <div className="hidden md:block">
+                            <Link
+                            href="#"
+                            className="h-9 w-9 ghost sm"
+                            >
+                                <LogOutIcon className="mr-2 h-4 w-4" />
+                                Signout
+                            </Link>
+                        </div>
+                    </nav>
+                </div>
+            </TooltipProvider> */}
         </div>
     )
 }
